bee188cf7da3b17185f703895e3d8d464739df9f
Nick Smith <Kremnari@gmail.com>
2021-10-21 12:29:52 -0600

    broke icon

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 export const TICKS_PER_SECOND = 30
 export const TICKS_MAX_PHASE = TICKS_PER_SECOND * 60 * 5 // 5 minutes
 export const IDB_SAVE_VERSION = "0.03"
-export const LAST_DS_DB = "21102021"
+export const LAST_DS_DB = "22102021"
