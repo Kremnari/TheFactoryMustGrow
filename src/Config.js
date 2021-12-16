60a815c002698a7c6cba80d9ae8d7b872fa333fd
Nick Smith <Kremnari@gmail.com>
2021-12-16 00:49:05 -0700

    DataSet update
    FacBlocks tech referenced outdated system name

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const TICKS_PER_SECOND = 30
 export const TICKS_MAX_PHASE = TICKS_PER_SECOND * 60 * 5 // 5 minutes
 export const IDB_SAVE_VERSION = "0.03"
-export const LAST_DS_DB = "20211201"
+export const LAST_DS_DB = "20211215"
 export const LAST_ICON_SET = "20211201"
 export const LAST_FORCE_RESET = "20211130"
